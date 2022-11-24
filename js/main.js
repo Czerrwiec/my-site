b54397489470098ed0be5eca50234abed557640f
Czerwiec <czerrwiec@gmail.com>
2022-11-24 17:51:30 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const nav = document.querySelector('.navigation');
+const navBtn = document.querySelector('.hamburger');
+const links = document.querySelectorAll('.nav__link')
+const hamburger = document.querySelector('.hamburger-inner')
+const footerYear = document.querySelector('.footer-year')
+
+
+
+const handleNav = () => {
+    navBtn.classList.toggle('is-active');
+    nav.classList.toggle('navigation--active');
+    // document.body.classList.toggle('sticky-body');
+    
+
+    // links.forEach(item => {
+    //     item.addEventListener('click', () => {
+    //         navMobile.classList.remove('nav-mobile--active');
+    //         navBtn.classList.remove('is-active');
+    //         document.body.classList.remove('sticky-body');
+    //     })
+    // })
+}
+
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
+
+
+
+navBtn.addEventListener('click', handleNav)
+
+handleCurrentYear()
