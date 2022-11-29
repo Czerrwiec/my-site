bf0e2fff22fd718762667e1d26ae07c899ddc60f
Czerwiec <czerrwiec@gmail.com>
2022-11-29 20:13:11 +0100

    added some features

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,13 @@ const footerYear = document.querySelector('.footer-year')
 const formBtn = document.querySelector('.form-btn')
 
 
+// PRÓBA
+const box = document.querySelector('.box');
+const squares = 7700;
+
+let sliderValue = 40;
+let range = 200
+//PRÓBA
 
 const handleNav = () => {
     navBtn.classList.toggle('is-active');
@@ -22,6 +29,10 @@ const handleNav = () => {
     // })
 }
 
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
 
 const addPulseAnimation = (e) => {
     const top = e.clientY
@@ -36,17 +47,70 @@ const addPulseAnimation = (e) => {
     e.target.appendChild(circle)
     circle.style.top = btnTopPosition +'px'
     circle.style.left = btnLeftPosition + 'px'
+
+    setTimeout(() => {
+        circle.remove()
+    }, 800);
 }
 
 
-const handleCurrentYear = () => {
-    const year = (new Date).getFullYear();
-    footerYear.innerText = year;
+// PRÓBA
+
+
+
+
+const createSquare = () => {
+	for (let i = 0; i < squares; i++) {
+		const square = document.createElement('div');
+		square.classList.add('square');
+		box.append(square);
+
+        square.addEventListener('mouseover', () => setColor(square))
+        square.addEventListener('mouseout', () => removeColor(square))
+
+	}
+};
+
+
+const setColor = (square) => {
+	
+	const h = Math.floor(Math.random() * 60) + range;
+
+	square.addEventListener('click', () => {
+		range = Math.floor(Math.random() * 360)
+	})
+
+	const s = sliderValue + '%';
+	const l = '50%';
+
+	square.style.backgroundColor = `hsl(${h},${s},${l})`;
+	square.classList.add('animation-rotate')
+
+	setTimeout(() => {
+		square.classList.remove('animation-rotate')
+	}, 2000);
+};
+
+const removeColor = (square) => {
+    square.style.backgroundColor = 'transparent'
 }
+
+
+
+
+
+
+
+
+
+
+// PRÓBA
+
 
 
 navBtn.addEventListener('click', handleNav)
 formBtn.addEventListener('click', addPulseAnimation)
 
+createSquare();
 
 handleCurrentYear()
