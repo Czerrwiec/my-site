61277ad93fa383051247a941e4db6fec63c24940
Czerwiec <czerrwiec@gmail.com>
2022-12-02 14:03:16 +0100

    media q added

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,47 @@
-const nav = document.querySelector('.navigation');
-const navBtn = document.querySelector('.hamburger');
-const links = document.querySelectorAll('.nav-link');
-const hamburger = document.querySelector('.hamburger-inner');
-const footerYear = document.querySelector('.footer-year');
-const formBtn = document.querySelector('.form-btn');
-const mobileNavigation = document.querySelector('.navigation');
+let nav;
+let navBtn;
+let links
+let hamburger
+let footerYear
+let formBtn
+let mobileNavigation
+let box
+
+const squares = 875;
+let sliderValue = 40;
+let range = 200;
+
+const main = () => {
+	prepareDOMElements();
+	prepareDOMEvents();
+	createSquare();
+	checkSlick();
+	handleCurrentYear();
+};
+
+const prepareDOMElements = () => {
+	nav = document.querySelector('.navigation');
+	navBtn = document.querySelector('.hamburger');
+	links = document.querySelectorAll('.nav-link');
+	hamburger = document.querySelector('.hamburger-inner');
+	footerYear = document.querySelector('.footer-year');
+	formBtn = document.querySelector('.form-btn');
+	mobileNavigation = document.querySelector('.navigation');
+	box = document.querySelector('.box');
+}
+
+const prepareDOMEvents = () => {
+	navBtn.addEventListener('click', handleNav);
+	formBtn.addEventListener('click', addPulseAnimation);
+	
+}
+
+
 
 const checkSlick = () => {
 	setInterval(() => {
 		const center = document.querySelectorAll('.slick-element');
+
 		center.forEach((item) => {
 			if (
 				item.classList.contains('slick-active') &&
@@ -30,37 +63,44 @@ const checkSlick = () => {
 				item.classList.contains('slick-active') &&
 				item.dataset.slickIndex === '2'
 			) {
-                item.style.color = 'gold';
-			} else if (item.classList.contains('slick-active') &&
-            item.classList.contains('slick-current') &&
-            item.classList.contains('slick-active') &&
-            item.dataset.slickIndex === '3') {
-                item.style.color = '#7f12f9';
-            } else if (item.classList.contains('slick-active') &&
-            item.classList.contains('slick-current') &&
-            item.classList.contains('slick-active') &&
-            item.dataset.slickIndex === '4') {
-                item.style.color = 'grey';
-            } else if (item.classList.contains('slick-active') &&
-            item.classList.contains('slick-current') &&
-            item.classList.contains('slick-active') &&
-            item.dataset.slickIndex === '5') {
-                item.style.color = '#f15a1f'
-            }
-            else {
-				item.style.color = 'antiquewhite';
-			}
+				item.style.color = 'gold';
+			} else if (
+				item.classList.contains('slick-active') &&
+				item.classList.contains('slick-current') &&
+				item.classList.contains('slick-active') &&
+				item.dataset.slickIndex === '3'
+			) {
+				item.style.color = '#7f12f9';
+			} else if (
+				item.classList.contains('slick-active') &&
+				item.classList.contains('slick-current') &&
+				item.classList.contains('slick-active') &&
+				item.dataset.slickIndex === '4'
+			) {
+				item.style.color = 'black';
+				item.children[0].style.textShadow = '0 0 2px white';
+			} else if (
+
+				item.classList.contains('slick-active') &&
+				item.classList.contains('slick-current') &&
+				item.classList.contains('slick-active') &&
+				item.dataset.slickIndex === '5'
+			) {
+				item.style.color = '#f15a1f';
+			} else if (
+				item.classList.contains('slick-active') &&
+				item.classList.contains('slick-current') &&
+				item.classList.contains('slick-active') &&
+				item.dataset.slickIndex === '6'
+			) {
+				item.style.color = '#d94446';
+			} else {
+				item.style.color = 'antiquewhite';	
+			}	
 		});
-	}, 100);
+	}, 150);
 };
 
-// PRÓBA
-const box = document.querySelector('.box');
-const squares = 875;
-
-let sliderValue = 40;
-let range = 200;
-//PRÓBA
 
 const handleNav = () => {
 	navBtn.classList.toggle('is-active');
@@ -100,6 +140,7 @@ const addPulseAnimation = (e) => {
 	}, 800);
 };
 
+
 // PRÓBA
 
 const createSquare = () => {
@@ -136,9 +177,5 @@ const removeColor = (square) => {
 
 // PRÓBA
 
-navBtn.addEventListener('click', handleNav);
-formBtn.addEventListener('click', addPulseAnimation);
 
-createSquare();
-checkSlick();
-handleCurrentYear();
+document.addEventListener('DOMContentLoaded', main);
--- src/js/slick.js
+++ src/js/slick.js
@@ -1,47 +1,57 @@
 $(document).ready(function () {
-	$('.slick-component').slick({
-		centerMode: true,
-		centerPadding: '10px',
-		slidesToShow: 3,
-		draggable: true,
+	$('.slick-component')
+		.slick({
+			centerMode: true,
+			centerPadding: '10px',
+			slidesToShow: 3,
 
-		 
-       
-		prevArrow:
-			"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-		nextArrow:
-			"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
-		responsive: [
-			{
-				breakpoint: 768,
-				settings: {
-					arrows: false,
-					centerMode: true,
-					centerPadding: '40px',
-					slidesToShow: 3,
+			prevArrow:
+				"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+			nextArrow:
+				"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+			responsive: [
+				{
+					breakpoint: 1024,
+					settings: {
+						centerMode: true,
+						centerPadding: '0px',
+						slidesToShow: 3,
+					},
 				},
-			},
-			{
-				breakpoint: 480,
-				settings: {
-					arrows: false,
-					centerMode: true,
-					centerPadding: '40px',
-					slidesToShow: 1,
-				},
-			},
-		],
-	}).on('beforeChange', (event, slick, currentSlide, nextSlide) => {
-		if (currentSlide !== nextSlide) {
-			document.querySelectorAll('.slick-center + .slick-cloned').forEach((next) => {
-				
-				setTimeout(() => next.classList.add('slick-current', 'slick-center'));
-				
-			});
-		}
-	});
 
+				{
+					breakpoint: 768,
+					settings: {
+						arrows: false,
+						autoplay: true,
+						autoplaySpeed: 2500,
+						centerMode: true,
+						centerPadding: '10px',
+						slidesToShow: 3,
+					},
+				},
+				{
+					breakpoint: 480,
+					settings: {
+						arrows: false,
+						autoplay: true,
+						autoplaySpeed: 2500,
+						centerMode: true,
+						centerPadding: '0px',
+						slidesToShow: 3,
+					},
+				},
+			],
+		})
+		.on('beforeChange', (event, slick, currentSlide, nextSlide) => {
+			if (currentSlide !== nextSlide) {
+				document
+					.querySelectorAll('.slick-center + .slick-cloned')
+					.forEach((next) => {
+						setTimeout(() =>
+							next.classList.add('slick-current', 'slick-center')
+						);
+					});
+			}
+		});
 });
-
-
-
