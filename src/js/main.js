feeacc6321c9cefe90afc28a2bfb1f9fcaaececb
Czerwiec <czerrwiec@gmail.com>
2022-12-09 17:43:22 +0100

    Added welcome animation and delete boxes animation

--- src/js/main.js
+++ src/js/main.js
@@ -5,16 +5,13 @@ let hamburger
 let footerYear
 let formBtn
 let mobileNavigation
-let box
 
-const squares = 875;
-let sliderValue = 40;
+
 let range = 200;
 
 const main = () => {
 	prepareDOMElements();
 	prepareDOMEvents();
-	createSquare();
 	checkSlick();
 	handleCurrentYear();
 };
@@ -27,7 +24,6 @@ const prepareDOMElements = () => {
 	footerYear = document.querySelector('.footer-year');
 	formBtn = document.querySelector('.form-btn');
 	mobileNavigation = document.querySelector('.navigation');
-	box = document.querySelector('.box');
 }
 
 const prepareDOMEvents = () => {
@@ -43,39 +39,45 @@ const checkSlick = () => {
 		const center = document.querySelectorAll('.slick-element');
 
 		center.forEach((item) => {
+			
 			if (
 				item.classList.contains('slick-active') &&
 				item.classList.contains('slick-current') &&
 				item.classList.contains('slick-active') &&
-				item.dataset.slickIndex === '0'
+				item.children[0].classList.contains('fa-html5')
+				// item.dataset.slickIndex === '0'
 			) {
 				item.style.color = '#e44d26';
 			} else if (
 				item.classList.contains('slick-active') &&
 				item.classList.contains('slick-current') &&
 				item.classList.contains('slick-active') &&
-				item.dataset.slickIndex === '1'
+				item.children[0].classList.contains('fa-css3-alt')
+				// item.dataset.slickIndex === '1'
 			) {
 				item.style.color = '#0096dc';
 			} else if (
 				item.classList.contains('slick-active') &&
 				item.classList.contains('slick-current') &&
 				item.classList.contains('slick-active') &&
-				item.dataset.slickIndex === '2'
+				item.children[0].classList.contains('fa-js')
+				// item.dataset.slickIndex === '2'
 			) {
 				item.style.color = 'gold';
 			} else if (
 				item.classList.contains('slick-active') &&
 				item.classList.contains('slick-current') &&
 				item.classList.contains('slick-active') &&
-				item.dataset.slickIndex === '3'
+				item.children[0].classList.contains('fa-bootstrap')
+				// item.dataset.slickIndex === '3'
 			) {
 				item.style.color = '#7f12f9';
 			} else if (
 				item.classList.contains('slick-active') &&
 				item.classList.contains('slick-current') &&
 				item.classList.contains('slick-active') &&
-				item.dataset.slickIndex === '4'
+				item.children[0].classList.contains('fa-github')
+				// item.dataset.slickIndex === '4'
 			) {
 				item.style.color = 'black';
 				item.children[0].style.textShadow = '0 0 2px white';
@@ -84,14 +86,16 @@ const checkSlick = () => {
 				item.classList.contains('slick-active') &&
 				item.classList.contains('slick-current') &&
 				item.classList.contains('slick-active') &&
-				item.dataset.slickIndex === '5'
+				item.children[0].classList.contains('fa-ubuntu')
+				// item.dataset.slickIndex === '5'
 			) {
 				item.style.color = '#f15a1f';
 			} else if (
 				item.classList.contains('slick-active') &&
 				item.classList.contains('slick-current') &&
 				item.classList.contains('slick-active') &&
-				item.dataset.slickIndex === '6'
+				item.children[0].classList.contains('fa-gulp')
+				// item.dataset.slickIndex === '6'
 			) {
 				item.style.color = '#d94446';
 			} else {
@@ -116,66 +120,29 @@ const handleNav = () => {
 	});
 };
 
-const handleCurrentYear = () => {
-	const year = new Date().getFullYear();
-	footerYear.innerText = year;
-};
 
 const addPulseAnimation = (e) => {
 	const top = e.clientY;
 	const left = e.clientX;
-
+	
 	const btnTopPosition = top - e.target.getBoundingClientRect().top;
 	const btnLeftPosition = left - e.target.offsetLeft;
-
+	
 	const circle = document.createElement('span');
 	circle.classList.add('circle');
-
+	
 	e.target.appendChild(circle);
 	circle.style.top = btnTopPosition + 'px';
 	circle.style.left = btnLeftPosition + 'px';
-
+	
 	setTimeout(() => {
 		circle.remove();
 	}, 800);
 };
 
-
-// PRÓBA
-
-const createSquare = () => {
-	for (let i = 0; i < squares; i++) {
-		const square = document.createElement('div');
-		square.classList.add('square');
-		box.append(square);
-
-		square.addEventListener('mouseover', () => setColor(square));
-		square.addEventListener('mouseout', () => removeColor(square));
-	}
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
 };
-
-const setColor = (square) => {
-	const h = Math.floor(Math.random() * 60) + range;
-
-	square.addEventListener('click', () => {
-		range = Math.floor(Math.random() * 360);
-	});
-	const s = sliderValue + '%';
-	const l = '50%';
-
-	square.style.backgroundColor = `hsl(${h},${s},${l})`;
-	// square.classList.add('animation-rotate')
-
-	// setTimeout(() => {
-	// 	square.classList.remove('animation-rotate')
-	// }, 2000);
-};
-
-const removeColor = (square) => {
-	square.style.backgroundColor = 'transparent';
-};
-
-// PRÓBA
-
 
 document.addEventListener('DOMContentLoaded', main);
